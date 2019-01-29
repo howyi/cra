5090ced146bb5298dbcc0f77f5737e37c2a8bf8e
howyi <qrbys@outlook.com>
2019-01-29 16:43:54 +0900

    実装予定のコマンドを仮追加

--- src/Console/InitCommand.php
+++ src/Console/InitCommand.php
@@ -1,21 +1,20 @@
 <?php
 
-namespace Howyi\Cra;
+namespace Howyi\Cra\Console;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class TestCommand extends Command
+class InitCommand extends Command
 {
     protected function configure()
     {
-        $this
-            ->setName('test');
+        $this->setName('init');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        dump('dump');
+        // TODO
     }
 }
