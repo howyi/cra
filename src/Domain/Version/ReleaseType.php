93f7488207898fd2b5135ba8b8930ab1a825eeba
Masaya Takeda <844196@gmail.com>
2019-01-30 16:32:57 +0900

    define release-type

---
+++ src/Domain/Version/ReleaseType.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Howyi\Cra\Domain\Version;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * ReleaseType
+ */
+final class ReleaseType extends Enum
+{
+    /**
+     * @var string
+     */
+    public const MAJOR = 'major';
+
+    /**
+     * @var string
+     */
+    public const MINOR = 'minor';
+
+    /**
+     * @var string
+     */
+    public const PATCH = 'patch';
+}
