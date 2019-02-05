f905726b9af31f8f90cc04024625d373d450792a
Masaya Takeda <844196@gmail.com>
2019-02-05 13:13:54 +0900

    リリースブランチ準備コマンドを仮実装 #15

--- src/App/Console/PrepareReleaseBranchCommand.php
+++ src/App/Console/PrepareReleaseBranchCommand.php
@@ -2,6 +2,11 @@
 
 namespace Sasamium\Cra\App\Console;
 
+use Cz\Git\GitRepository;
+use Sasamium\Cra\App\Adapter\PrepareReleaseBranchAdapter;
+use Sasamium\Cra\Core\ReleaseType;
+use Sasamium\Cra\Core\UseCase\PrepareReleaseBranch;
+use Sasamium\Cra\Core\Version;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -30,6 +35,21 @@ class PrepareReleaseBranchCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // TODO
+        // configureでREQUIREDしているため、必ずstringが返る
+        /** @var string $releaseTypeOrVersion */
+        $releaseTypeOrVersion = $input->getArgument('New version');
+        $releaseType = ReleaseType::memberByValueWithDefault($releaseTypeOrVersion, null);
+        if (is_null($releaseType)) {
+            if (Version::isValidString($releaseTypeOrVersion)) {
+                throw new \RuntimeException('バージョン指定での準備はまだサポートされていない');
+            }
+            throw new \RuntimeException('不正なリリースタイプを渡された');
+        }
+
+        // TODO: 844196 設定を参照するようにする
+        $adapter = new PrepareReleaseBranchAdapter(new GitRepository(getcwd()), 'release/', 'v', 'master');
+        $useCase = new PrepareReleaseBranch($adapter);
+
+        $useCase->run($releaseType);
     }
 }
