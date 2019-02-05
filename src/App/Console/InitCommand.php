0848def9f34c095e86bf37e3434c6e03e19b6db6
howyi <qrbys@outlook.com>
2019-02-05 15:58:00 +0900

    add php-school/cli-menu

--- src/App/Console/InitCommand.php
+++ src/App/Console/InitCommand.php
@@ -5,6 +5,8 @@ namespace Sasamium\Cra\App\Console;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use PhpSchool\CliMenu\CliMenu;
+use PhpSchool\CliMenu\Builder\CliMenuBuilder;
 
 class InitCommand extends Command
 {
@@ -15,6 +17,20 @@ class InitCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // TODO
+        $itemCallable = function (CliMenu $menu) {
+            echo $menu->getSelectedItem()->getText();
+        };
+
+        $menu = (new CliMenuBuilder())
+            ->setTitle('cra Configure')
+            ->addItem('Initialize', $itemCallable)
+            ->addItem('Change', $itemCallable)
+            ->addLineBreak('-')
+            ->setBorder(1, 2, 'green')
+            ->setPadding(2, 4)
+            ->setMarginAuto()
+            ->build();
+
+        $menu->open();
     }
 }
